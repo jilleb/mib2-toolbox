72853e008def3c25cf706251c0ac41f3a8fb68dd
jilleb <jjberends@gmail.com>
2019-11-11 15:25:19 +0100

    Make extract tools compatible with Python3

--- Tools/extract-canim_vw.py
+++ Tools/extract-canim_vw.py
@@ -1,11 +1,11 @@
 #----------------------------------------------------------
-#--- Quick 'n' dirty MIF file compressor
+#--- Quick 'n' dirty CANIM file compressor
 #
 # File:        extract_canim
 # Author:      Jille
-# Revision:    3
+# Revision:    4
 # Purpose:     Extract canim startup images files. This version works for VW
-# Comments:     Usage: extract_canim.py <filename> <outdir>
+# Comments:    Usage: extract_canim.py <filename> <outdir>
 #----------------------------------------------------------
 
 import struct
@@ -21,7 +21,7 @@ except ImportError:
   pip install image""")
 
 if len(sys.argv) != 3:
-  print 'usage: extract_canim.py <filename> <outdir>'
+  print ('usage: extract_canim.py <filename> <outdir>')
   sys.exit(1)
 
 out_dir = sys.argv[2]
@@ -35,25 +35,20 @@ offset = 0
 (magic,) = struct.unpack_from('<8s', data, offset)
 offset = offset + 8
 
-if magic != 'ANIM1   ':
-  print 'incorrect magic!'
-  sys.exit(1)
-
-
 (stage_width, stage_height, cmdblock_len, unk) = struct.unpack_from('<LLLL', data, offset)
 offset = offset + 16
 
-print "stage_width: %d\nstage_height: %d\ncmdblock_len: %d\n" %(stage_width, stage_height, cmdblock_len)
+print ("stage_width: %d\nstage_height: %d\ncmdblock_len: %d\n" %(stage_width, stage_height, cmdblock_len))
 
 (cmd_code, img_num, img_width, img_height, bytes_per_pixel, data_start) = struct.unpack_from('<LLLLLL', data, offset)
 
 while cmd_code == 0x11:
-  print 'generating img_%d from offset %d \t  size: %d x %d \t bpp: %d \t size: %d bytes' % (img_num, cmdblock_len+data_start+32, img_width, img_height, bytes_per_pixel, img_width*img_height*bytes_per_pixel)
+  print ('generating img_%d from offset %d \t  size: %d x %d \t bpp: %d \t size: %d bytes' % (img_num, cmdblock_len+data_start+32, img_width, img_height, bytes_per_pixel, img_width*img_height*bytes_per_pixel))
   
   if bytes_per_pixel == 0x3:
-	im = Image.frombuffer('RGB', (img_width, img_height), data[0x20+cmdblock_len+data_start:], 'raw', 'RGB', 0, 1)
+    im = Image.frombuffer('RGB', (img_width, img_height), data[0x20+cmdblock_len+data_start:], 'raw', 'RGB', 0, 1)
   if bytes_per_pixel == 0x4:
-	im = Image.frombuffer('RGBA', (img_width, img_height), data[0x20+cmdblock_len+data_start:], 'raw', 'RGBA', 0, 1)
+    im = Image.frombuffer('RGBA', (img_width, img_height), data[0x20+cmdblock_len+data_start:], 'raw', 'RGBA', 0, 1)
 
   im.save(os.path.join(out_dir, 'img_%d.png'%img_num))
   offset = offset + 0x20
--- Tools/extract-mcf.py
+++ Tools/extract-mcf.py
@@ -3,7 +3,7 @@
 #
 # File:        extract-mcf.py
 # Author:      Jille, sVn
-# Revision:    2
+# Revision:    3
 # Purpose:     Discover Pro MIB2 mcf file exporter
 # Comments:    Usage: extract-mcf.py <filename> <outdir>
 # Credits:     Partially based on code supplied by booto @ 
@@ -22,7 +22,15 @@ try:
 except ImportError:
   sys.exit("""  You are missing the PIL module!
   install it by running: 
-  pip install image""")
+  pip install image
+  """)
+
+try:
+  from progressbar import ProgressBar, Percentage, Bar
+except ImportError:
+  sys.exit("""  You are missing the progressbar module!
+  install it by running: 
+  pip install progressbar""")
 
 if len(sys.argv) != 3:
   print ("usage: extract-mcf.py <filename> <outdir>")
@@ -39,13 +47,14 @@ counterL = 0
 counterLA = 0
 
 (magic,) = struct.unpack_from('<4s', data, offset) # '<4s'  =  '<' little-endian, 's' is type char, '6s' Array of 6 chars; get first entry of the returned tuple
-if magic != '\x89\x4d\x43\x46': # corresponds to MCF file starting
-	print 'incorrect magic!'
-	sys.exit(1)
+#print (magic)
+#if magic != '\x89\x4d\x43\x46': # corresponds to MCF file starting
+#	print ('incorrect magic!')
+#	sys.exit(1)
 	
 offset = 32
 (size_of_TOC,) = struct.unpack_from('<I', data, offset) 
-print 'size of TOC: ' + str(size_of_TOC)
+print ('size of TOC: ' + str(size_of_TOC))
 
 data_start = size_of_TOC + 56
 print ("data start: %d"%(data_start))
@@ -58,13 +67,11 @@ print ("number of files: %d"%(num_files))
 offset = 52
 for image_id in range (0, int(num_files)):
 	(file_type, file_id, file_offset, file_size) = struct.unpack_from('<4sIII', data, offset) # file_size = meta information (size of 40) + zsize
-	print ("filetype %s file_id %d offset %d size %d"%(file_type, file_id, file_offset, file_size)) 
+	#print ("filetype %s file_id %d offset %d size %d"%(file_type, file_id, file_offset, file_size)) 
 	offset = offset + 16
 
-
-
 offset = data_start
-
+pbar = ProgressBar(widgets=[Percentage(), Bar()], maxval=num_files).start()
 for image_id in range (0, int(num_files)):
 #for image_id in range (0, 2):
 	(type, file_id, always_8, zsize, max_pixel_count, always_1, unknown_16, width, height, image_mode, always__1) = struct.unpack_from('<4sIIIIIIhhhh', data, offset)
@@ -75,7 +82,7 @@ for image_id in range (0, int(num_files)):
 	#offsethex = "%0.8X" % zlib_data_offset
 	zlib_image = data[zlib_data_offset:zlib_data_offset+zsize]
 	zlib_decompress = zlib.decompress(zlib_image)
-	print ("extracting %s;%d;%d;%d;%d;%d;%d"%(type, file_id, zsize, max_pixel_count, width, height, unknown_16 ))
+	#print ("extracting %s;%d;%d;%d;%d;%d;%d"%(type, file_id, zsize, max_pixel_count, width, height, unknown_16 ))
 	try:
 		if (image_mode == 4096):
 			im = Image.frombuffer('L', (width, height), zlib_decompress, 'raw', 'L', 0, 1)
@@ -88,6 +95,7 @@ for image_id in range (0, int(num_files)):
 		print ("error on %s;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d"%(type, file_id, always_8, zsize, max_pixel_count, always_1, unknown_16, width, height, image_mode, always__1))
 
 	offset = offset+zsize+40
+	pbar.update(image_id)
 
 counter = counterL+counterLA+counterRGBA
 rest = int(num_files) - counter
@@ -95,4 +103,3 @@ print("\nExtracting %s done\n%d of %d images were extracted" %(sys.argv[1], coun
 print("%d RGBA images\n%d LA-mode images\n%d L-mode images"%(counterRGBA, counterLA, counterL))
 if rest > 0:
 	print("%d were not exported for some reason" %(rest))
-
