7aa7992b8311c9410ceea3c0c7f8a74ba42a8350
jilleb <jjberends@gmail.com>
2020-01-09 16:28:58 +0100

    Added LSD.jxe and GEM.jar dump and import

---
+++ Phone/Apps/0/default/dump_lsdjxe.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+#info
+TOPIC=LSD
+DESCRIPTION="This script will dump lsd.jxe"
+
+#Firmware/unit info:
+VERSION="$(cat /net/rcc/dev/shmem/version.txt | grep "Current train" | sed 's/Current train = //g' | sed -e 's|["'\'']||g' | sed 's/\r//')"
+FAZIT=$(cat /tmp/fazit-id);
+
+echo $DESCRIPTION
+echo FAZIT of this unit: $FAZIT
+echo Firmware version: $VERSION
+echo "---------------------------"
+sleep .5
+
+
+#Is there any SD-card inserted?
+if [ -d /net/mmx/fs/sda0 ]; then
+    echo SDA0 found
+    VOLUME=/net/mmx/fs/sda0
+elif [ -d /net/mmx/fs/sdb0 ] ; then
+    echo SDB0 found
+    VOLUME=/net/mmx/fs/sdb0
+else 
+    echo No SD-cards found.
+    exit 0
+fi
+
+sleep .5
+
+echo Mounting SD-card.
+mount -uw $VOLUME
+
+sleep .5
+
+#Make backup folder
+DUMPFOLDER=$VOLUME/Dump/$VERSION/$FAZIT/$TOPIC
+
+echo Dump-folder: $DUMPFOLDER
+
+mkdir -p $DUMPFOLDER
+echo "Please wait while the file is dumped"
+echo "It will appear like nothing is happening."
+sleep 1
+echo
+echo "Dumping, this will take a while. Please be patient."
+
+cp /net/mmx/ifs/lsd.jxe $DUMPFOLDER/lsd.jxe
+
+# Make readonly again
+mount -ur $VOLUME
+
+echo "Done. LSD.jxe dump can be found in the Dump folder on your SD-card"
+
+exit 0
