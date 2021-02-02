f81fda9e3ec1df5e8f80eacad3e55e7a9207249e
olli991 <55631413+olli991@users.noreply.github.com>
2021-02-02 19:48:18 +0100

    Language copy - Added language copy script for skin1-5 - Added input path for language files on Custom/Language - Added new GreenMenu for Language import

---
+++ Toolbox/scripts/install_skin4_language.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+export SKINNAME=skin4
+export TOPIC=Language
+export MIBPATH=/eso/hmi/lsd/Resources/$SKINNAME/i18n/*.res
+export SDPATH=/$TOPIC/$SKINNAME/*.res
+export DESCRIPTION="This script will copy and replace language files"
+export TYPE="file"
+
+echo $DESCRIPTION
+. /eso/hmi/engdefs/scripts/mqb/util_info.sh
+. /eso/hmi/engdefs/scripts/mqb/util_mountsd.sh
+if [[ -z "$VOLUME" ]] 
+then
+	echo "No SD-card found, quitting"
+	exit 0
+fi
+
+export BACKUPFOLDER=$VOLUME/Backup/$VERSION/$FAZIT/$TOPIC/$SKINNAME
+. /eso/hmi/engdefs/scripts/mqb/util_backup.sh
+. /eso/hmi/engdefs/scripts/mqb/util_copy.sh
+echo "Done. Now restart the unit."
+exit 0
