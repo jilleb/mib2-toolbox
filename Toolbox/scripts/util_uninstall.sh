da98b135efc58d27cd8840b219aa9ac8236580b2
olli991 <55631413+olli991@users.noreply.github.com>
2021-02-07 01:05:57 +0100

    Adding demo GreenMenus to uninstall script

--- Toolbox/scripts/util_uninstall.sh
+++ Toolbox/scripts/util_uninstall.sh
@@ -10,7 +10,7 @@ echo $DESCRIPTION
 mount -uw /mnt/app
 
 echo "Deleting old mqbcoding.esd pre v4.1"
-rm /mnt/app/eso/hmi/engdefs/mqbcoding.esd
+rm -v /mnt/app/eso/hmi/engdefs/mqbcoding.esd
 rm -rv /mnt/app/eso/hmi/engdefs/mqbcoding.esd.*
 
 echo "Deleting Toolbox GreenMenus"
@@ -18,10 +18,10 @@ rm -rv /mnt/app/eso/hmi/engdefs/mqb*.esd
 rm -rv /mnt/app/eso/hmi/engdefs/mqb*.esd.*
 
 echo "Deleting Toolbox Demo GreenMenus, if installed"
-rm -rv /mnt/app/eso/hmi/engdefs/Demo.esd
-rm -rv /mnt/app/eso/hmi/engdefs/Demo_sub.esd
-rm -rv /mnt/app/eso/hmi/engdefs/example.esd
-rm -rv /mnt/app/eso/hmi/engdefs/mqbcoding_tests.esd
+rm -v /mnt/app/eso/hmi/engdefs/Demo.esd
+rm -v /mnt/app/eso/hmi/engdefs/Demo_sub.esd
+rm -v /mnt/app/eso/hmi/engdefs/example.esd
+rm -v /mnt/app/eso/hmi/engdefs/mqbcoding_tests.esd
 
 echo "Deleting old MIB Toolbox scripts pre v4.1"
 rm -rv /mnt/app/eso/bin/PhoneCustomer/*.sh
