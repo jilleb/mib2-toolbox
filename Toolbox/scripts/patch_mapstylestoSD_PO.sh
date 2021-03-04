dc38e574ff0fce270e9a69bd3406e5adc220a741
Jille <jjberends@gmail.com>
2021-03-04 10:42:35 +0100

    First version of SD Mapstyles feature

---
+++ Toolbox/scripts/patch_mapstylestoSD_PO.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+#info
+export TOPIC=AndroidAuto
+export DESCRIPTION="This script will move Porsche mapstyles to SD"
+
+
+echo $DESCRIPTION
+
+. /eso/hmi/engdefs/scripts/mqb/util_info.sh
+. /eso/hmi/engdefs/scripts/mqb/util_mountsd.sh
+
+sleep .5
+
+mount -uw $VOLUME
+
+sleep .5
+
+
+mount -uw /mnt/app
+echo "Setting LoadStylesFromSDCard flag"
+touch /navigation/LoadStylesFromSDCard
+
+mount -uw /mnt/system
+echo "Mounting Porsche mapstyles iso"
+mount -t cd /net/mmx.mibhigh.net/mnt/navdb/database/eu/mapStyles/7/navigation_styles_PO_EU.iso /mnt/tmp
+echo "Copying Porsche mapstyles files to SD"
+cp -R -V /mnt/tmp/ $VOLUME
+
+
+#make readonly again
+mount -ur /mnt/app
+mount -ur $VOLUME
+mount -ur /mnt/system
+
+echo "Done, please restart"
+
+exit 0
