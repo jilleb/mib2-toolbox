1dc8d97df3e058fc210d7273c9ad61094cc3e27a
Andrew Leech <andrew@alelec.net>
2021-08-09 14:38:01 +1000

    Add echo outputs on the set/unset skipMost scripts. Currently there's no output so it's hard to know if it's busy / done / failed.

--- Toolbox/scripts/set_skipMostPopup.sh
+++ Toolbox/scripts/set_skipMostPopup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 mount -uw /net/rcc/mnt/efs-persist
+echo "Creating: /net/rcc/mnt/efs-persist/SWDL/skipMostPopup.txt"
 touch /net/rcc/mnt/efs-persist/SWDL/skipMostPopup.txt
+echo "Done."
 exit 0
 
--- Toolbox/scripts/unset_skipMostPopup.sh
+++ Toolbox/scripts/unset_skipMostPopup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 mount -uw /net/rcc/mnt/efs-persist
+echo "Removing: /net/rcc/mnt/efs-persist/SWDL/skipMostPopup.txt"
 rm /net/rcc/mnt/efs-persist/SWDL/skipMostPopup.txt
+echo "Done."
 exit 0
 
